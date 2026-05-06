d91e75e6ad11fc573dac0f756f12bec37aee1719
nurill <mnurilhuda2005@gmail.com>
2026-05-06 18:38:59 +0700

    Menambahkan file migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('role');
+            $table->string('cv_path')->nullable();
+            $table->string('linkedin_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
