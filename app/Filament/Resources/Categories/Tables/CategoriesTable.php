c0e3b6847368394162ee94957ed9bf38733ae43f
nurill <mnurilhuda2005@gmail.com>
2026-05-06 20:14:08 +0700

    Membuat File Category Table

--- app/Filament/Resources/Categories/Tables/CategoriesTable.php
+++ app/Filament/Resources/Categories/Tables/CategoriesTable.php
@@ -3,9 +3,11 @@
 namespace App\Filament\Resources\Categories\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
 
 class CategoriesTable
 {
@@ -13,13 +15,27 @@ class CategoriesTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->label('Nama Kategori')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('slug')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Tanggal Dibuat')
+                    ->dateTime('d M Y')
+                    ->sortable(),
             ])
+            ->defaultSort('name', 'asc')
             ->filters([
                 //
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
