16760ee7f41d34eac323fa16bb7d4a7e44a55cf0
RizkyFirdiansyah <mr.frdsyh@gmail.com>
2026-05-06 21:37:40 +0700

    Menambahkan resource collaborator

---
+++ app/Filament/Resources/Collaborators/Schemas/CollaboratorForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Collaborators\Schemas;
+
+use Filament\Schemas\Schema;
+
+class CollaboratorForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
