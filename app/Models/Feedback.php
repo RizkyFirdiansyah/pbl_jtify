d94891a7891aee518e23385f3677574da80250f2
RizkyFirdiansyah <mr.frdsyh@gmail.com>
2026-05-21 17:53:36 +0700

    Menambahkan resource feedback

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Feedback extends Model
 {
 
-    protected $fillable = ['user_id', 'message'];
+    protected $fillable = ['user_id', 'message', 'status'];
 
     public function user()
     {
