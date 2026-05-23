01f537124d8a05b8bec640fdc2c7665e2df8570f
RizkyFirdiansyah <mr.frdsyh@gmail.com>
2026-05-23 16:56:32 +0700

    Modifikasi file migration article

--- database/migrations/2026_05_23_094128_create_articles_table.php
+++ database/migrations/2026_05_23_094128_create_articles_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('content');
+            $table->string('slug')->unique();
+            $table->string('poster_path')->nullable();
+            $table->enum('status', ['draft', 'published', 'pending_review', 'archived'])->default('draft');
+            $table->foreignId('approved_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamp('approved_at')->nullable();
+            $table->text('revision_notes')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
