05ac28e2fdbdee3ad4578438849fed11b10f72f0
RizkyFirdiansyah <mr.frdsyh@gmail.com>
2026-05-23 16:43:21 +0700

    Menambahkan route API

---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookmarkController;
+use App\Http\Controllers\FeedbackController;
+use App\Http\Controllers\InformationController;
+use App\Http\Controllers\InterestController;
+use App\Http\Controllers\UserProfileController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::get('/informations', [InformationController::class, 'index']);
+Route::get('/informations/categories', [InformationController::class, 'categories']);
+Route::get('/informations/{information}', [InformationController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return response()->json([
+            'success' => true,
+            'data' => $request->user(),
+        ]);
+    });
+
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::get('/auth/me', [AuthController::class, 'me']);
+
+    Route::get('/bookmarks', [BookmarkController::class, 'index']);
+    Route::post('/bookmarks/toggle', [BookmarkController::class, 'toggle']);
+    Route::get('/bookmarks/check', [BookmarkController::class, 'check']);
+    Route::patch('/bookmarks/reminder', [BookmarkController::class, 'updateReminder']);
+
+    Route::get('/interests', [InterestController::class, 'index']);
+    Route::post('/interests/toggle', [InterestController::class, 'toggle']);
+    Route::get('/interests/check', [InterestController::class, 'check']);
+    Route::get('/interests/information/{information}', [InterestController::class, 'getByInformation']);
+
+    Route::get('/feedbacks', [FeedbackController::class, 'index']);
+    Route::post('/feedbacks', [FeedbackController::class, 'store']);
+    Route::get('/feedbacks/{id}', [FeedbackController::class, 'show']);
+    Route::match(['put', 'patch'], '/feedbacks/{id}', [FeedbackController::class, 'update']);
+    Route::delete('/feedbacks/{id}', [FeedbackController::class, 'destroy']);
+
+    Route::get('/profile', [UserProfileController::class, 'show']);
+    Route::get('/profile/edit', [UserProfileController::class, 'edit']);
+    Route::put('/profile', [UserProfileController::class, 'update']);
+    Route::delete('/profile/cv', [UserProfileController::class, 'deleteCv']);
+    Route::get('/profile/feedbacks', [UserProfileController::class, 'feedbacks']);
+});
